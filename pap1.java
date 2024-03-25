a2eac24c4fb44d0f0fd31bea1a3903582b3b69bb
Alex Martinefsky <129073991+alexfandelcafe@users.noreply.github.com>
2024-03-25 16:34:10 -0300

    Add files via upload

---
+++ pap1.java
@@ -0,0 +1,22 @@
+package ymepasaabuscar;
+
+import java.util.Scanner;
+
+public class pap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numero = 0;
+		
+	  Scanner sc = new Scanner(System.in);
+	  
+	  System.out.println("Numero");
+	  
+		numero= sc.nextInt();
+		
+		System.out.println("numero es " + numero );
+
+	}
+
+}
---
+++ pap2.java
@@ -0,0 +1,22 @@
+package ymepasaabuscar;
+
+import java.util.Scanner;
+
+public class pap2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numero = 0;
+		
+	  Scanner sc = new Scanner(System.in);
+	  
+	  System.out.println("Edad");
+	  
+		numero = (sc.nextInt() + 1) ;
+		
+		System.out.println("edad año proximo es " + numero );
+
+	}
+
+}
